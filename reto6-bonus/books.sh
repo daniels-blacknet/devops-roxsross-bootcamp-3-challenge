62950342a7b70ee6a8de113766940de5ea3a9b44
daniels-blacknet <danielsanti.ar@gmail.com>
2022-08-24 01:54:25 -0300

    Modificiones varias para cargar coleccion books

--- reto6-bonus/books.sh
+++ reto6-bonus/books.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "########### Loading data to Mongo DB ###########"
-mongoimport --jsonArray --db Library --collection books --file /docker-entrypoint-initdb.d/books.json
+# No funca:
+##mongoimport --jsonArray --db Library --collection books -u root -p toor -authenticationDatabase admin /docker-entrypoint-initdb.d/books.json
+mongoimport --host 127.0.0.1 --db Library --collection books --type json -u root -p Aa123456 --authenticationDatabase=admin --file /docker-entrypoint-initdb.d/books.json
+echo "########### Loaded data to Mongo DB Library DB, books collection ############"
