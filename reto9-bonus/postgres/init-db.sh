b3f676fa3e42f58198afb185c6b6ffa463086ffe
daniels-blacknet <danielsanti.ar@gmail.com>
2022-08-30 19:39:19 -0300

    Cambios en init-db.sh y Dockerfile

--- reto9-bonus/postgres/init-db.sh
+++ reto9-bonus/postgres/init-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER myapp password myapp123;
+    CREATE USER myapp;
     CREATE DATABASE myappdb;
     GRANT ALL PRIVILEGES ON DATABASE myappdb TO myapp;
 
