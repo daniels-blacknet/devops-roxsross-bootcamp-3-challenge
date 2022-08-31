a614e8a50f2df74ab4de73cd33d61059e46cb078
daniels-blacknet <danielsanti.ar@gmail.com>
2022-08-31 02:18:15 -0300

    Modificaciones a Dockerfiles y docker-compose. Creado init-db2.sh con modif. Modificada myapp.py por errores de ejecucion en conexion a base. Definicion fuera de lugar de 'connection'

--- reto9-bonus/myapp/myapp.py
+++ reto9-bonus/myapp/myapp.py
@@ -2,13 +2,16 @@ import psycopg2
 import os
 from flask import Flask
 myapp = Flask(__name__)
-connection = None
 
 @myapp.route('/')
 def home():
-    global connection
-    try:
-        connection = psycopg2.connect(dbname=os.getenv('POSTGRES_DB'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PASSWORD'), host=os.getenv('POSTGRES_HOST'), port=os.getenv('POSTGRES_PORT'))
+    dbname = os.getenv('POSTGRES_DB')
+    user = os.getenv('POSTGRES_USER')
+    password=os.getenv('POSTGRES_PASSWORD')
+    host=os.getenv('POSTGRES_HOST')
+    print (dbname, user, password, host)
+    connection = psycopg2.connect(dbname=os.getenv('POSTGRES_DB'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PASSWORD'), host=os.getenv('POSTGRES_HOST'))
+    try: 
         cursor = connection.cursor()
         query = "select * from tabla;"
 
