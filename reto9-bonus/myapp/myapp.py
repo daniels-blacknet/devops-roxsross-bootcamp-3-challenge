3f4c865c4e6fb80595a4df3d22c20e2a1d7d919e
daniels-blacknet <danielsanti.ar@gmail.com>
2022-08-30 23:50:19 -0300

    Agregar docker-compose.yml e wait-for-it.sh

--- reto9-bonus/myapp/myapp.py
+++ reto9-bonus/myapp/myapp.py
@@ -2,9 +2,11 @@ import psycopg2
 import os
 from flask import Flask
 myapp = Flask(__name__)
+connection = None
 
 @myapp.route('/')
 def home():
+    global connection
     try:
         connection = psycopg2.connect(dbname=os.getenv('POSTGRES_DB'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PASSWORD'), host=os.getenv('POSTGRES_HOST'), port=os.getenv('POSTGRES_PORT'))
         cursor = connection.cursor()
